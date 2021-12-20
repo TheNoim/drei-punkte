35fa3c305f0bef02f9c06402360b039fc3cb13e0
Nils Bergmann <nilsbergmann@noim.io>
2021-12-20 21:17:29 +0100

    Remove Main.jar and reduce motor speed

--- src/com/dreipunkte/Main.java
+++ src/com/dreipunkte/Main.java
@@ -6,19 +6,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            public void run() {
-                SteeringControl.stop();
-            }
-        }));
-		
+			public void run() {
+				SteeringControl.stop();
+			}
+		}));
+
 		SteeringControl.goForward();
-		
+
 		Delay.msDelay(2000);
-		
+
 		SteeringControl.turnLeft(45);
-		
+
 		Delay.msDelay(2000);
-		
+
 		SteeringControl.stop();
 	}
 
--- src/com/dreipunkte/SteeringControl.java
+++ src/com/dreipunkte/SteeringControl.java
@@ -5,23 +5,22 @@ import lejos.hardware.port.MotorPort;
 public class SteeringControl {
 	public static final ExtendedEV3LargeRegulatedMotor motorRight = new ExtendedEV3LargeRegulatedMotor(MotorPort.C);
 	public static final ExtendedEV3LargeRegulatedMotor motorLeft = new ExtendedEV3LargeRegulatedMotor(MotorPort.B);
-	
-	
+
 	private static int radius = 14; // 13cm
 	private static double maxMotorArac = 5.969; // 5.969 for 360 degree motor spinn
 	private static double motorRadius = 0.95; // 0.95 cm
-	private static float speed = 1000;
-	
+	private static float speed = 500;
+
 	private static double realMaxDegree = 56.0;
-	
+
 	public static void goForward() {
 		motorRight.setSpeed(speed);
 		motorLeft.setSpeed(speed);
-		
+
 		motorRight.forward();
 		motorLeft.forward();
 	}
-	
+
 	public static void stop() {
 		motorRight.stop();
 		motorLeft.stop();
@@ -30,42 +29,43 @@ public class SteeringControl {
 	public static void turnLeft(int angle) {
 		turn(motorRight, motorLeft, angle);
 	}
-	
+
 	public static void turnRight(int angle) {
 		turn(motorLeft, motorRight, angle);
 	}
-	
-	private static void turn(ExtendedEV3LargeRegulatedMotor activeMotor, ExtendedEV3LargeRegulatedMotor passiveMotor, int angle) {
+
+	private static void turn(ExtendedEV3LargeRegulatedMotor activeMotor, ExtendedEV3LargeRegulatedMotor passiveMotor,
+			int angle) {
 		passiveMotor.stop(true);
 		activeMotor.stop(true);
-		
+
 		MotorState lastActive = activeMotor.getLastState();
 		MotorState lastPassive = passiveMotor.getLastState();
-		
+
 		// b = ((PI * r) / 180 degree) * alpha
-		
+
 		// Small degree correction
 		int correctedAngle = correctedAngle(angle);
-				
-		double b =  (double) (((Math.PI * radius) / 180) * correctedAngle); 
-		
+
+		double b = (double) (((Math.PI * radius) / 180) * correctedAngle);
+
 		double neededRotationFactor = b / maxMotorArac;
-		
+
 		double neededRotation = neededRotationFactor * maxMotorArac;
-		
+
 		// alpha = b / ( (PI * r) / 180 degree );
-		
-		int motorAngleToTurn = (int) Math.round( neededRotation / ((Math.PI * motorRadius) / 180) );
-		
+
+		int motorAngleToTurn = (int) Math.round(neededRotation / ((Math.PI * motorRadius) / 180));
+
 		passiveMotor.flt(true);
-		
+
 		activeMotor.rotate(motorAngleToTurn);
-		
+
 		activeMotor.doNewState(lastActive, true);
-		
+
 		passiveMotor.doNewState(lastPassive, true);
 	}
-	
+
 	private static int correctedAngle(int angle) {
 		return (int) Math.round((realMaxDegree / 90.0) * angle);
 	}
